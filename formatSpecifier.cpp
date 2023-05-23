1fc59091547bb990493ed92b7502af3197e0344e
DaraSingh2020 <dara.singh.sangha@gmail.com>
2023-05-23 19:17:31 -0400

    update

---
+++ formatSpecifier.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+
+int main() {
+
+char CC;
+printf("Please enter a charactor: ");
+scanf("%c", &CC);
+printf("The charactor you entered was: %c\n", CC);
+
+double T;
+printf("Please enter any number: ");
+scanf("%lf", &T);
+printf("The number you entered was: %lf", T);
+
+
+return 0;
+}

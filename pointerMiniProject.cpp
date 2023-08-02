6bf2b9a1d01729036ff268795270d402653088ab
DaraSingh2020 <dara.singh.sangha@gmail.com>
2023-08-02 12:35:32 -0400

    update

---
+++ pointerMiniProject.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int arr[],const size_t sizeA){
+    for(int i{0}; i<sizeA; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int* apply_all(int* a1,const size_t a1_size, int* a2,const size_t a2_size){
+    int* result{nullptr};
+    result=new int[a1_size*a2_size];
+    int counter{0};
+    for(size_t i{0}; i<a1_size; i++){
+        for(size_t j{0}; j<a2_size; j++){
+            result[counter]=*(a1+i)*(*(a2+j));
+            counter++;
+        }
+    }
+    return result;
+}
+
+int main(){
+
+const size_t array1_size{5};
+const size_t array2_size{3};
+
+int array1[]{1,2,3,4,5};
+int array2[]{10,20,30};
+
+cout << "Array: ";
+print(array1,array1_size);
+
+cout << "Array: ";
+print(array2,array2_size);
+
+int* result = apply_all(array1,array1_size, array2, array2_size);
+constexpr size_t result_size{array1_size*array2_size};
+
+cout << "Result: ";
+print(result, result_size);
+
+cout << endl;
+
+
+return 0;
+}
